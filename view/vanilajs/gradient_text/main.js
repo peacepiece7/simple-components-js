3489e0c42da9262bc618e1983e5a45cd335150c6
taetae <peacepiece7@naver.com>
2023-01-28 22:39:44 +0900

    feat : gredient text 예제 추가

--- view/vanilajs/gradient_text/main.js
+++ view/vanilajs/gradient_text/main.js
@@ -1,10 +1,12 @@
 const dealy = async (ms) => await new Promise((res) => setTimeout(res, ms))
 
 async function changeTextBgColor() {
-  for(let i = 0 ; i < 100; i++) {
+  for (let i = 0; i < 100; i++) {
     await dealy(2000)
     document.querySelector('.gradientText')?.classList.toggle('transitionOn')
+    Array.from(document.querySelectorAll('.gradirent h1')).map((e, i) => {
+      setTimeout(() => e.classList.toggle('transitionOn'), 500 * i)
+    })
   }
 }
 changeTextBgColor()
-
